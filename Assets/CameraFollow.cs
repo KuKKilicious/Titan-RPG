b21cc01352df585bef8c711059e4dec71a70ca35
KuKKilicious <clouseyda@yahoo.de>
2018-12-03 17:12:14 +0100

    Prototyping first gameplay feel &Adding Easy Notes Asset Pack

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    
+    Transform playerToFollow;
+    private void Start() {
+        playerToFollow = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void LateUpdate() {
+        transform.position = playerToFollow.position;
+    }
+}
