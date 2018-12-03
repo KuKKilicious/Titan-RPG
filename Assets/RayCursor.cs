b79211ba6c3a29969970206b13802c12a1d2b19b
KuKKilicious <clouseyda@yahoo.de>
2018-12-03 23:03:57 +0100

    Using Raycasts To Query Click

---
+++ Assets/RayCursor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCursor : MonoBehaviour {
+    CameraRaycaster raycaster;
+	// Use this for initialization
+	void Start () {
+        raycaster = GetComponent<CameraRaycaster>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(raycaster.layerHit);
+	}
+}
