5bee82acf65179dd56a3c7a73a1e8c5d0c2f4e2e
KuKKilicious <clouseyda@yahoo.de>
2019-01-14 12:21:57 +0100

    added more Enemies into the world, and implemented Text Appearance Trigger

---
+++ Assets/TextAppearance.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+public class TextAppearance : MonoBehaviour
+{
+    [SerializeField] [TextArea] string textToDisplay;
+    [SerializeField] bool playOnce;
+
+
+    bool hasPlayed;
+
+
+    UIManager uiManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManager = FindObjectOfType<UIManager>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>())
+        {
+            if (playOnce && hasPlayed)
+            {
+                return;
+            }
+            else
+            {
+                uiManager.DisplayText(textToDisplay);
+                hasPlayed = true;
+            }
+
+        }
+    }
+}
--- Assets/_Core/UIManager.cs
+++ Assets/_Core/UIManager.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField]
-    GameObject deathText;
+    [SerializeField] GameObject deathText;
+    [SerializeField] Text generalText;
+    [SerializeField] float textSpeedPerChar = 0.1f;
+
+    private void Start()
+    {
+        generalText.text = "";
+    }
 
     public void PlayDeathScreen()
     {
@@ -14,5 +21,29 @@ public class UIManager : MonoBehaviour
         deathText.gameObject.SetActive(true);
     }
 
+
+    public void DisplayText(string text)
+    {
+        StartCoroutine(DisplayTextString(text));
+    }
+
+    private IEnumerator DisplayTextString(string text)
+    {
+        generalText.text = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            generalText.text += text[i];
+            yield return new WaitForSeconds(textSpeedPerChar);
+
+        }
+        StartCoroutine(ClearText(2f)); // todo implement non-conflicting way of handling text with multiple Triggers, (Queue?)
+    }
+
+    private IEnumerator ClearText(float v)
+    {
+        yield return new WaitForSeconds(v);
+        generalText.text = "";
+    }
 }
+
 
